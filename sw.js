ed6d7d2ff9f67c03cbe1a2a8852faf30dcb226f1
WaxTag <279438988+raulrotman-commits@users.noreply.github.com>
2026-09-18 15:44:01 +0300

    Update site (202609181544)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Keeps the app working without a connection, e.g. in a club with no signal.
 // Bump VERSION whenever the site is published again, so visitors get the new files.
-const VERSION="vinyl-labels-v1";
+const VERSION="vinyl-labels-202609181544";
 const FILES=["./", "index.html", "manifest.webmanifest", "vendor/qrcode.min.js", "fonts/fonts.css", "fonts/2sDcZGJYnIjSi6H75xkzZmW5Kb8VZBHR.woff2", "fonts/2sDcZGJYnIjSi6H75xkzaGW5Kb8VZA.woff2", "fonts/2sDcZGJYnIjSi6H75xkzamW5Kb8VZBHR.woff2", "fonts/tss0ApVBdCYD5Q7hcxTE1ArZ0bb-iXxw2d8oBxk.woff2", "fonts/tss0ApVBdCYD5Q7hcxTE1ArZ0bb_iXxw2d8oBxk.woff2", "fonts/tss0ApVBdCYD5Q7hcxTE1ArZ0bbwiXxw2d8o.woff2", "icons/icon-180.png", "icons/icon-192.png", "icons/icon-32.png", "icons/icon-512.png", "icons/icon.svg"];
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(VERSION).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
